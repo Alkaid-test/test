4797389bf29ed4c7d5a765f576b628a5017755b3
Yu Negishi <negishi.y.ad@m.titech.ac.jp>
2014-05-23 17:52:57 +0900

    negi

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+//negi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
