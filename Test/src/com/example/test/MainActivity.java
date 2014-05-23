7e41653e0f3e27ff7e7d6deeead79ea7faebcdfc
yk <hitfpt23@gmail.com>
2014-05-23 18:01:12 +0900

    [

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// ohoh
 //test1
 //test2
         if (savedInstanceState == null) {
